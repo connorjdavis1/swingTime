b5d6716b2d3fb612890a3c3d8fa60e2547518ff8
connorjdavis1 <connorjdavis1@gmail.com>
2018-01-27 17:07:35 -0600

    add exploratory r script

---
+++ preliminaryJazzomatExplore.R
@@ -0,0 +1,4 @@
+# THIS SCRIPT WILL SERVE TO COMPUTE MY EXPLORATION OF JAZZ TIMING BEFORE I BUILD MY CORPUS
+# IT CONTAINS THE FOLLOWING PROCESSES
+
+#=====================================================================================#
